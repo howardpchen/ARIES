f0a9af22abef9b344384d67ee8e3202f6932fb3f
howardpchen <sleepy.howard@gmail.com>
2016-04-01 08:27:08 -0400

    fixed database login

--- src/com/howardpchen/aries/dao/Database.java
+++ src/com/howardpchen/aries/dao/Database.java
@@ -2,14 +2,14 @@ package com.howardpchen.aries.dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
- 
+
 public class Database {
  
     public static Connection getConnection() {
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            Connection con = DriverManager.getConnection("jdbc:mysql://107.170.166.121/ariesdb?useSSL=false",
-                  "ssarda", "$nCsioa@qF0&1");
+            Connection con = DriverManager.getConnection(AriesDatabaseConfig.DB_URL, 
+            		AriesDatabaseConfig.DB_USERNAME, AriesDatabaseConfig.DB_PASSWORD);
             //Connection con = DriverManager.getConnection("jdbc:mysql://localhost/ariesdb?useSSL=false",
              //      "root", "$nCsioa@qF0&1");
             return con;
